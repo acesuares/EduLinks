6efff5e9af87876975dd5a52129621b9bcc49c06
Ace Suares <ace@suares.an>
2018-11-11 18:15:05 -0400

    css for page; page works in non-subdomain

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -20,7 +20,14 @@ class LinksController < InlineFormsController
     render layout: 'homepage'
   end
 
-  def pooh
+  def page
+    @group = Group.find_by(slug: params[:group_slug])
+    redirect_to root_url(:host => request.domain) and return if @group.nil?
+    @topic = Topic.find_by(slug: params[:topic_slug])
+    redirect_to root_url(:host => request.domain) and return if @topic.nil?
+    @sub_topic = SubTopic.find_by(slug: params[:sub_topic_slug])
+    redirect_to root_url(:host => request.domain) and return if @sub_topic.nil?
+    render layout: 'homepage'
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
   def application_title
     'CurEduLinks'
   end
+  def display_link(link)
+    content_tag(:div,
+      content_tag(:h1, link.name) +
+      content_tag(:p, link.description) +
+      link_to(link.name, link.link),
+    class: :link_block)
+  end
 end
