2063fca578426b4b70304aae5a7eb2d42c758bcb
Ace Suares <ace@suares.an>
2018-11-22 00:20:10 -0400

    css auto height

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,8 @@ module ApplicationHelper
   end
   def display_link(link)
     content_tag(:div,
-      content_tag(:h1, link.name) +
-      content_tag(:p, link.description) +
+      content_tag(:h1, link.name, class: :link_name_show) +
+      content_tag(:p, link.description, class: :link_description_show) +
       link_to(link.name, link.link),
     class: :link_block)
   end
--- config/initializers/inline_forms.rb
+++ config/initializers/inline_forms.rb
@@ -4,3 +4,6 @@ JA_NEE_ONBEKEND = JA_NEE.merge({ 4 => 'onbekend'})
 
 SLUG_REGEX = /[a-z][a-z0-9]|[a-z][a-z0-9_-]+[a-z0-9]/
 USERNAME_REGEX = /[a-z][a-z0-9_-]+[a-z0-9]/
+
+MAX_LINK_NAME_LENGTH = 20
+ELLIPSIS = '...'
